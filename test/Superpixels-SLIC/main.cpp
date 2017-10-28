6f06cd4ecffe42bd0ac330bad8dd38f1f4237290
kaiwenChang <kaiwen.chang@mines-paristech.fr>
2017-10-28 16:44:28 +0200

    Add SLIC algorithm

---
+++ test/Superpixels-SLIC/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/core/core.hpp"
+#include <string>
+
+#include "Superpixels.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char* argv[]){
+
+    if(argc != 2){
+        cout << "Usage: ./superpixels <input_img>" << endl;
+        return 0;
+    }
+
+    Mat img = imread(argv[1]);
+
+    if(!img.data){
+        cout << "Bad image ..." << endl;
+        return 0;
+    }
+    
+    imshow("Original", img);
+
+    blur(img, img, Size(3,3));
+    imshow("Blurred", img);
+    
+    Superpixels sp(img);
+    
+    Mat boundaries = sp.viewSuperpixels();
+    imshow("Superpixel boundaries", boundaries);
+    imwrite("boundaries.png", boundaries);
+    
+    Mat recolored = sp.colorSuperpixels();
+    imshow("Average superpixel colors", recolored);
+    imwrite("recolored.png", recolored);
+
+    // centers of the K clusters
+    vector<Point> centers = sp.getCenters();
+    
+    waitKey(0);
+
+    return 0;
+}
